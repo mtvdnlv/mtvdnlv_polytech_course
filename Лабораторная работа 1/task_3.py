fd435ca4eda2efaed9c80dcf08569db73ed93a8c
mtvdnlv <danilovmatveika@gmail.com>
2024-10-18 19:06:26 +0300

    Delete Лабораторная работа 1 directory

--- Лабораторная работа 1/task_3.py
+++
@@ -1,9 +0,0 @@
-list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
-
-Middle = len(list_players)//2  #найдем середину списка
-
-first_team = list_players[:Middle] #первая половина участников
-second_team = list_players[Middle:] #вторая половина
-
-print(first_team)
-print(second_team)
